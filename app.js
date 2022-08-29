0d96ed9d7cb4136fcb9225a64740dff4d1731825
ANDREW JAMES BOYLE <ANDREWBOYLEMUSIC@GMAIL.COM>
2022-08-29 16:32:41 -0700

    auth function fixed

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ import { renderProject } from './render-utils.js';
 
 /*  "boiler plate" auth code */
 // checking if we have a user! (will redirect to auth if not):
-// checkAuth();
+checkAuth();
 // can optionally return the user:
 // const user = checkAuth();
 
 // sign out link:
-// const signOutLink = document.getElementById('sign-out-link');
-// signOutLink.addEventListener('click', signOutUser);
+const signOutLink = document.getElementById('sign-out-link');
+signOutLink.addEventListener('click', signOutUser);
 /* end "boiler plate auth code" */
 
 // grab needed DOM elements on page:
